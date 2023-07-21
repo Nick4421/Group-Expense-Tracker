d4a56e7a0b917e792cc5d9fe74b77ea75fa170a6
Nick4421 <110129335+Nick4421@users.noreply.github.com>
2023-07-21 16:08:52 -0400

    More work on the gui

--- visuals.py
+++ visuals.py
@@ -1,29 +1,29 @@
 import PySimpleGUI as sg
 
 # Sample data for the list
-list_data = ['Item 1', 'Item 2', 'Item 3', 'Item 4', 'Item 5', 'Item 6', 'Item 7', 'Item 8', 'Item 9', 'Item 10',
-             'Item 11', 'Item 12', 'Item 13', 'Item 14', 'Item 15', 'Item 16', 'Item 17', 'Item 18', 'Item 19', 'Item 20']
+list_data = ['Item 1', 'Item 2', 'Item 3', 'Item 4', 'Item 5']
 
-# Define the layout with expandable elements
+# Define the layout with the specified components
 layout = [
-    [sg.Text('Scrollable List Example', font=('Helvetica', 20), expand_x=True)],
-    [sg.Listbox(values=list_data, size=(30, 10), enable_events=True,
-                key='-LIST-', expand_x=True, expand_y=True)],
-    [sg.Button('Submit', size=(10, 2))]
+    [sg.Text('Title of the Project', font=('Helvetica', 20))],
+    [
+        sg.Listbox(values=list_data, size=(30, 10),
+                   enable_events=True, key='-LIST-'),
+        sg.Column([
+            [sg.Button('Button')],
+            [sg.Text('Some Text')],
+        ], expand_y=True, element_justification='center')
+    ]
 ]
 
-# Create the window with grab_anywhere option for resizing
-window = sg.Window('Scrollable List', layout,
-                   resizable=True, grab_anywhere=True)
+# Create the window
+window = sg.Window('Project Layout', layout)
 
 # Event loop
 while True:
     event, values = window.read()  # type: ignore
     if event == sg.WINDOW_CLOSED:
         break
-    elif event == 'Submit':
-        selected_items = values['-LIST-']
-        sg.popup(f'Selected item(s): {", ".join(selected_items)}')
 
 # Close the window
 window.close()
