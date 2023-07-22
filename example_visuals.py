3c42e19335bb6bd84978a84b0138715c0c9e7ac2
Nick4421 <110129335+Nick4421@users.noreply.github.com>
2023-07-21 22:49:02 -0400

    Got page switching to work for the gui

--- example_visuals.py
+++ example_visuals.py
@@ -1,17 +1,39 @@
 import PySimpleGUI as sg
-# set the theme for the screen/window
-sg.theme("DarkTanBlue")
-# define layout
-sz = (10, 20)
-col1 = [[sg.Text('Column1', background_color='red', size=sz)]]
-col2 = [[sg.Text('Column2', background_color='green', size=sz)]]
-col3 = [[sg.Text('Column3', background_color='yellow', size=sz)]]
-col4 = [[sg.Text('Column4', background_color='blue', size=sz)]]
 
-layout = [[sg.Column(col1, element_justification='c'), sg.Column(col2, element_justification='c'), sg.Column(
-    col3, element_justification='c'), sg.Column(col4, element_justification='c')]]
+# ----------- Create the 3 layouts this Window will display -----------
+layout1 = [[sg.Text('Enter your name and email address to register')],
+           [sg.Text('Name: '), sg.Input(key='-NAME-')],
+           [sg.Text('Email Address: '), sg.Input(key='-EMAIL_ADDRESS-')]]
 
-window = sg.Window("Column and Frame", layout)
-event, values = window.read()  # type: ignore
+layout2 = [[sg.Text('Enter your vote:')],
+           [sg.Radio('Trump', 'vote')],
+           [sg.Radio('Biden', 'vote')]]
 
+layout3 = [[sg.Text('Thanks for voting!')]]
+
+# ----------- Create actual layout using Columns and a row of Buttons
+layout = [[sg.Column(layout1, key='-COL1-'),
+           sg.Column(layout2, visible=False, key='-COL2-'),
+           sg.Column(layout3, visible=False, key='-COL3-')],
+          [sg.Button('Next'), sg.Button('Return to Start'), sg.Button('Exit')]]
+
+window = sg.Window('Dynamic Window', layout)
+
+layout = 1  # The currently visible layout
+while True:
+    event, values = window.read()  # type: ignore
+    if event in (None, 'Exit'):
+        break
+    if event == 'Next':
+        print(layout)
+        window[f'-COL{layout}-'].update(visible=False)
+        if layout < 3:
+            layout += 1
+            window[f'-COL{layout}-'].update(visible=True)
+        else:
+            window[f'-COL3-'].update(visible=True)
+    elif event == 'Return to Start':
+        window[f'-COL{layout}-'].update(visible=False)
+        window[f'-COL1-'].update(visible=True)
+        layout = 1
 window.close()
--- visuals.py
+++ visuals.py
@@ -3,27 +3,39 @@ import PySimpleGUI as sg
 # Sample data for the list
 list_data = ['Item 1', 'Item 2', 'Item 3', 'Item 4', 'Item 5']
 
-# Define the layout with the specified components
-layout = [
-    [sg.Text('Title of the Project', font=('Helvetica', 20))],
-    [
-        sg.Listbox(values=list_data, size=(30, 10),
-                   enable_events=True, key='-LIST-'),
-        sg.Column([
-            [sg.Button('Button')],
-            [sg.Text('Some Text')],
-        ], expand_y=True, element_justification='center')
-    ]
+main_menu_layout = [
+    [sg.Listbox(values=list_data, size=(55, 25), enable_events=True,
+                key='-LIST-', expand_y=True, expand_x=True, font=('Helvetica', 18)),
+     sg.VSeparator(),
+     sg.Column([
+         [sg.Button('Add Expense', size=(15, 2))],
+         [sg.Listbox(values=list_data, size=(35, 5), enable_events=True,
+                     key='-LIST2-', expand_y=True, expand_x=True, font=('Helvetica', 18))],
+         [sg.Text('Text 1', font=('Helvetica', 15)),
+             sg.Text('Text 2', font=('Helvetica', 15))],
+     ], expand_y=True, expand_x=True, element_justification='center')]
 ]
 
-# Create the window
-window = sg.Window('Project Layout', layout)
+add_expense_layout = [[sg.Text('Add Expense')]]
+
+# Define the layout with the specified components
+layout = [
+    [sg.Text('Group Name', font=('Helvetica', 20), expand_x=True)],
+    [sg.Column(main_menu_layout, key='-MAIN_MENU-'),
+     sg.Column(add_expense_layout, key='-ADD_EXPENSE-', visible=False)]
+]
+
+# Create the resizable window
+window = sg.Window('Group Expense Tracker', layout, resizable=True)
 
 # Event loop
 while True:
     event, values = window.read()  # type: ignore
     if event == sg.WINDOW_CLOSED:
         break
+    if event == 'Add Expense':
+        window['-MAIN_MENU-'].update(visible=False)
+        window['-ADD_EXPENSE-'].update(visible=True)
 
 # Close the window
 window.close()
